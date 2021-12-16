073b2b4cdb597e31c8746a38dcfc9c2f045a05df
Marshanda30 <marshandasyahlamitha@gmail.com>
2021-12-16 14:02:46 +0700

    get post data

--- src/components/layout/BarChart.vue
+++ src/components/layout/BarChart.vue
@@ -1,47 +1,21 @@
 <script>
-  import { Bar } from 'vue-chartjs'
+  import {
+    Line
+  } from 'vue-chartjs'
 
   export default {
-    extends: Bar,
+    extends: Line,
     data() {
       return {
         chartData: {
-          labels: ["2015-01", "2015-02", "2015-03", "2015-04", "2015-05", "2015-06", "2015-07", "2015-08", "2015-09",
-            "2015-10", "2015-11", "2015-12"
-          ],
+          labels: ["2021-12-10 1.50 PM", "2021-12-10 8.54 AM", "2021-12-10 8.59 AM", "2021-12-16 10.31 AM"],
           datasets: [{
-            label: 'Bar Chart',
+            label: 'Line Chart',
+            fill: false,
+            borderColor: 'black',
+            backgroundColor: 'white',
             borderWidth: 1,
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-            ],
-            pointBorderColor: '#2554FF',
-            data: [12, 19, 3, 5, 2, 3, 20, 3, 5, 6, 2, 1]
+            data: [220, 220, 210, 90]
           }]
         },
         options: {
@@ -61,7 +35,11 @@
             }]
           },
           legend: {
-            display: true
+            display: true,
+            labels: {
+              fontColor: "black",
+              fontSize: 14
+            },
           },
           responsive: true,
           maintainAspectRatio: false
